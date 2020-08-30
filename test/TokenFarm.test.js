ca85e5d846f84cefb31054cf51709ccab9005fc8
caltiva@gmail.com <caltiva@gmail.com>
2020-08-29 23:43:29 -0700

    Changes

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -43,7 +43,19 @@ contract('TokenFarm', ([owner, investor]) => {
   describe('Token Farm deployment', async () => {
     it('has a name', async () => {
       const name = await tokenFarm.name()
-      assert.equal(name, 'Dapp Token Farm')
+      assert.equal(name, 'Dapp Token Farm', "Got " + name + " for 'name'.")
+    })
+
+    it('contract has tokens', async () => {
+      let balance = await dappToken.balanceOf(tokenFarm.address)
+      assert.equal(balance.toString(), tokens('1000000'))
+    })
+  })
+
+  describe('Token Farm deployment', async () => {
+    it('has a name', async () => {
+      const name = await tokenFarm.name()
+      assert.equal(name, 'Dapp Token Farm', "Got " + name + " for 'name'.")
     })
 
     it('contract has tokens', async () => {
@@ -55,55 +67,59 @@ contract('TokenFarm', ([owner, investor]) => {
   describe('Farming tokens', async () => {
 
     it('rewards investors for staking mDai tokens', async () => {
-      let result
-
+     
       // Check investor balance before staking
-      result = await daiToken.balanceOf(investor)
-      assert.equal(result.toString(), tokens('100'), 'investor Mock DAI wallet balance correct before staking')
-
-      // Stake Mock DAI Tokens
-      await daiToken.approve(tokenFarm.address, tokens('100'), { from: investor })
-      await tokenFarm.stakeTokens(tokens('100'), { from: investor })
-
-      // Check staking result
-      result = await daiToken.balanceOf(investor)
-      assert.equal(result.toString(), tokens('0'), 'investor Mock DAI wallet balance correct after staking')
-
-      result = await daiToken.balanceOf(tokenFarm.address)
-      assert.equal(result.toString(), tokens('100'), 'Token Farm Mock DAI balance correct after staking')
-
-      result = await tokenFarm.stakingBalance(investor)
-      assert.equal(result.toString(), tokens('100'), 'investor staking balance correct after staking')
-
-      result = await tokenFarm.isStaking(investor)
-      assert.equal(result.toString(), 'true', 'investor staking status correct after staking')
-
-      // Issue Tokens
-      await tokenFarm.issueTokens({ from: owner })
-
-      // Check balances after issuance
-      result = await dappToken.balanceOf(investor)
-      assert.equal(result.toString(), tokens('100'), 'investor DApp Token wallet balance correct affter issuance')
-
-      // Ensure that only onwer can issue tokens
-      await tokenFarm.issueTokens({ from: investor }).should.be.rejected;
-
-      // Unstake tokens
-      await tokenFarm.unstakeTokens({ from: investor })
-
-      // Check results after unstaking
-      result = await daiToken.balanceOf(investor)
-      assert.equal(result.toString(), tokens('100'), 'investor Mock DAI wallet balance correct after staking')
-
-      result = await daiToken.balanceOf(tokenFarm.address)
-      assert.equal(result.toString(), tokens('0'), 'Token Farm Mock DAI balance correct after staking')
-
-      result = await tokenFarm.stakingBalance(investor)
-      assert.equal(result.toString(), tokens('0'), 'investor staking balance correct after staking')
-
-      result = await tokenFarm.isStaking(investor)
-      assert.equal(result.toString(), 'false', 'investor staking status correct after staking')
+      let result = await daiToken.balanceOf(investor)
+      console.log(result.toString());
+      assert.equal(result.toString(), tokens('101'), 'investor Mock DAI wallet balance correct before staking')
     })
+    it('contract has tokensx', async () => {
+      let balance = await dappToken.balanceOf(tokenFarm.address)
+      assert.equal(balance.toString(), tokens('1000000'))
+    })
+      // // Stake Mock DAI Tokens
+      // await daiToken.approve(tokenFarm.address, tokens('100'), { from: investor })
+      // await tokenFarm.stakeTokens(tokens('100'), { from: investor })
+
+      // // // Check staking result
+      // result = await daiToken.balanceOf(investor)
+      // assert.equal(result.toString(), tokens('0'), 'investor Mock DAI wallet balance correct after staking')
+
+      // result = await daiToken.balanceOf(tokenFarm.address)
+      // assert.equal(result.toString(), tokens('100'), 'Token Farm Mock DAI balance correct after staking')
+
+      // result = await tokenFarm.stakingBalance(investor)
+      // assert.equal(result.toString(), tokens('100'), 'investor staking balance correct after staking')
+
+      // result = await tokenFarm.isStaking(investor)
+      // assert.equal(result.toString(), 'true', 'investor staking status correct after staking')
+
+      // // Issue Tokens
+      // await tokenFarm.issueTokens({ from: owner })
+
+      // // Check balances after issuance
+      // result = await dappToken.balanceOf(investor)
+      // assert.equal(result.toString(), tokens('100'), 'investor DApp Token wallet balance correct affter issuance')
+
+      // // Ensure that only onwer can issue tokens
+      // await tokenFarm.issueTokens({ from: investor }).should.be.rejected;
+
+      // // Unstake tokens
+      // await tokenFarm.unstakeTokens({ from: investor })
+
+      // // Check results after unstaking
+      // result = await daiToken.balanceOf(investor)
+      // assert.equal(result.toString(), tokens('100'), 'investor Mock DAI wallet balance correct after staking')
+
+      // result = await daiToken.balanceOf(tokenFarm.address)
+      // assert.equal(result.toString(), tokens('0'), 'Token Farm Mock DAI balance correct after staking')
+
+      // result = await tokenFarm.stakingBalance(investor)
+      // assert.equal(result.toString(), tokens('0'), 'investor staking balance correct after staking')
+
+      // result = await tokenFarm.isStaking(investor)
+      // assert.equal(result.toString(), 'false', 'investor staking status correct after staking')
+
   })
 
 })
--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,21 @@
 require('babel-register');
 require('babel-polyfill');
-
+const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*" // Match any network id
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/HWFSPST1NUDSHSEQ4T9DCQC5KCEH6GZA9C")
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
+    },
+  
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
